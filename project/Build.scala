98f1be9a8547ae224e8490744c3d45cdf339cfa9
Trygve Laugstøl <trygvis@inamo.no>
2011-11-01 20:09:32 +0100

    o Trying to use the sbt-release plugin.

--- project/Build.scala
+++ project/Build.scala
@@ -1,4 +1,9 @@
 import sbt._
+import sbtrelease.Release
+import sbtrelease.Release._
+import sbtrelease.ReleaseKeys
+import sbtrelease.ReleasePart
+//import sbtrelease._
 import Keys._
 
 object JavaZonePortal extends Build {
@@ -9,23 +14,58 @@ object JavaZonePortal extends Build {
 
   lazy val buildSettings = Defaults.defaultSettings ++ Seq(
     organization := "no.java.javazone-portal",
-    version := "1.0-SNAPSHOT",
     scalaVersion := "2.9.1"
   )
+
+// Add back when we're deploying to oss.sonatype.com
+//  lazy val credentialsSetting = credentials ++
+//    (Seq("REPOSITORY_USER", "REPOSITORY_PASSWORD").map(k => Option(System.getenv(k))) match {
+//      case Seq(Some(user), Some(pass)) =>
+//        Seq(Credentials("javaBin", "smia.java.no", user, pass))
+//      case _ =>
+//        sys.error("foo") // Seq.empty[Credentials]
+//    })
+//    publishTo <<= (version) {
+//      version: String =>
+//        if (version.trim.endsWith("SNAPSHOT"))
+//          Some("javaBin" at "http://smia.java.no/maven/dist/snapshots")
+//        else
+//          Some("javaBin" at "http://smia.java.no/maven/dist/releases")
+//    },
+
 
   lazy val project = Project(
     id = "javazone-portal",
     base = file("."),
-    settings = buildSettings ++ Seq(
+    settings = buildSettings ++
+      releaseSettings ++
+//      credentialsSetting ++
+      Seq(
       description := "JavaZone 2012 Portal",
+      ReleaseKeys.releaseProcess <<= thisProjectRef apply { ref =>
+        import sbtrelease.ReleaseStateTransformations._
+        Seq[ReleasePart](
+          initialGitChecks,
+          checkSnapshotDependencies,
+          inquireVersions,
+          runTest,
+          setReleaseVersion,
+          commitReleaseVersion,
+          tagRelease,
+        // Enable when we're deploying to Sonatype
+  //        releaseTask(publish in Global in ref),
+          setNextVersion,
+          commitNextVersion
+        )
+      },
       libraryDependencies := Seq(
         "net.liftweb" %% "lift-common" % liftVersion % "compile->default" withSources(),
         "net.liftweb" %% "lift-webkit" % liftVersion % "compile->default" withSources(),
         "net.liftweb" %% "lift-mapper" % liftVersion % "compile->default" withSources(),
         "net.liftweb" %% "lift-util" % liftVersion % "compile->default" withSources(),
 
-        "no.javabin" %% "atom2twitterpublisher" % "1.1-SNAPSHOT",
-        "no.arktekk.atom-client" %% "atom-client-lift" % "1.0-SNAPSHOT",
+        "no.javabin" %% "atom2twitterpublisher" % "1.1",
+        "no.arktekk.atom-client" %% "atom-client-lift" % "1.0",
 
         "log4j" % "log4j" % "1.2.16",
         "org.slf4j" % "slf4j-log4j12" % "1.6.1",
