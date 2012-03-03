112a2883c1fc95f7496cbacf4ec02af706f8cd5d
olav <olav@bjorkoy.com>
2012-03-03 13:10:38 +0100

    Start på submeny.

--- src/main/scala/no/java/jzportal/html/default.scala
+++ src/main/scala/no/java/jzportal/html/default.scala
@@ -68,6 +68,15 @@ object default {
     </div>
 
     <div id="main">
+
+      <div id="submenu">
+        <a href="#">Practicalities</a>
+        <a href="#">Foobar</a>
+        <a href="#">Something</a>
+        <a href="#">Another</a>
+        <a href="#">Coolbreeze</a>
+      </div>
+      
       <div class="body hyphenate">
         {content}
       </div>
