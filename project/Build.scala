62a77556413d494277e9e8113cdc5390b9cb4205
Trygve Laugstøl <trygvis@inamo.no>
2012-03-03 10:41:37 +0100

    o Removing unused dependency.

--- project/Build.scala
+++ project/Build.scala
@@ -65,7 +65,6 @@ object JavaZonePortal extends Build {
         "net.databinder" %% "unfiltered-filter" % unfilteredVersion,
         "net.databinder" %% "unfiltered-jetty" % unfilteredVersion % "test",
 
-        "no.javabin" %% "atom2twitterpublisher" % "1.1",
         "no.arktekk.atom-client" %% "atom-client-core" % "1.1-SNAPSHOT",
 
         "org.constretto" %% "constretto-scala" % "1.0",
